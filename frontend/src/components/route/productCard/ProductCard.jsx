056ec56f716ba9fde18c52fa20c551580ba49b94
Prince-Lartey <lartey.princ@gmail.com>
2025-01-22 19:27:33 +0000

    displaying reviews and ratings for products

--- frontend/src/components/route/productCard/ProductCard.jsx
+++ frontend/src/components/route/productCard/ProductCard.jsx
@@ -9,7 +9,7 @@ import { addTocart } from "../../../redux/actions/cart.js";
 import { toast } from "react-toastify";
 import { addToWishlist, removeFromWishlist } from "../../../redux/actions/wishlist.js"
 
-const ProductCard = ({ data }) => {
+const ProductCard = ({ data, isEvent  }) => {
     const { cart } = useSelector((state) => state.cart)
     const { wishlist } = useSelector((state) => state.wishlist);
     const [click, setClick] = useState(false);
@@ -63,7 +63,7 @@ const ProductCard = ({ data }) => {
                 <h5 className={`${styles.shop_name}`}>{data.shop.name}</h5>
             </Link>
 
-            <Link to={`/product/${data._id}`}>
+            <Link to={`${isEvent === true ? `/product/${data._id}?isEvent=true` : `/product/${data._id}`}`}>
                 <h4 className="pb-3 font-[500]">
                     {data.name.length > 40 ? data.name.slice(0, 40) + "..." : data.name}
                 </h4>
