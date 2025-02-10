7a822761d8d838bb735936852479ea9fbfd02ebd
Prince-Lartey <lartey.princ@gmail.com>
2025-02-10 13:46:47 +0000

    fixed the best event bug

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,12 +1,32 @@
+import { useSelector } from "react-redux"
 import EventCard from "../components/events/EventCard"
+import Footer from "../components/layout/Footer"
 import Header from "../components/layout/Header"
+import Loader from "../components/layout/Loader"
 
 const EventsPage = () => {
+    const { allEvents, isLoading } = useSelector((state) => state.events)
+
     return (
-        <div>
-            <Header activeHeading={4} />
-            <EventCard active={true} />
-        </div>
+        <>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <div>
+                    <Header activeHeading={4} />
+                    <div className="w-full flex flex-wrap gap-4 justify-center my-4">
+                        {allEvents && allEvents.length > 0 ? (
+                            allEvents.map((event) => (
+                                <EventCard key={event._id} active={true} data={event} />
+                            ))
+                        ) : (
+                            <p className="text-center text-lg font-semibold">No events available</p>
+                        )}
+                    </div>
+                    <Footer />
+                </div>
+            )}
+        </>
     )
 }
 
