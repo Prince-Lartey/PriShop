b8aba50aa3c1924980205d62fb1e97536b5722ec
Prince-Lartey <lartey.princ@gmail.com>
2025-02-17 21:52:05 +0000

    deployment bug fixed 2

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173", credentials: true,}))
+app.use(cors({origin: "https://prishop-brown.vercel.app/", credentials: true,}))
 app.use("/", express.static("uploads"))
 app.use("/test", (req, res) => {
     res.send("Hello world!");
